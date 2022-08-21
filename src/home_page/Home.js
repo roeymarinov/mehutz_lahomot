f4d56f3ed7cfc224161535a7d86b7fef517633f6
roeymarinov <roeymarinov@gmail.com>
2022-08-22 00:29:33 +0300

    Home.js

--- src/home_page/Home.js
+++ src/home_page/Home.js
@@ -1,9 +1,45 @@
 import "./Home.css"
 import Header from "../header/Header";
+import eilatLogo from "../assets/logo_eilat.png"
+
+
+
 function Home() {
+    const opponentName = "הפועל אילת"
+    const gameTime = "20:00"
+    const busTime = "18:00"
+    const gameDate = "12/11"
+    const gameDay = "יום שני"
+    const court = "פיס ארנה"
+    
     return (
         <div className="Home">
             <Header/>
+            <div className="Content">
+                <text className="NextGameText">
+                    ההסעה הבאה:
+                </text>
+                <div className="GameCard">
+                    <img className="OpponentLogo" src={eilatLogo} alt="opponent logo"/>
+                    <div className="GameInfo">
+                        <h3 className="OpponentName">
+                            {opponentName}
+                        </h3>
+                        <text>
+                            {gameDay} {gameDate}
+                        </text>
+                        <text>
+                            משחק: {gameTime}
+                        </text>
+                        <text>
+                            הסעה: {busTime}
+                        </text>
+                        <text>
+                            {court}
+                        </text>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
